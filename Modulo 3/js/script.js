2276764f885fe05ec7336ba3aa2964ede2af532d
Ilde Stramandinoli <109019707+ildestram@users.noreply.github.com>
2023-03-10 20:21:26 +0100

    ok

--- Modulo 3/js/script.js
+++ Modulo 3/js/script.js
@@ -2,12 +2,16 @@
 
 const primoClick = document.getElementById("search-icon");
 const secondoClick = document.getElementById("search-component");
+// costante per l'animazione
 const sections = document.querySelectorAll(".reveal-section");
 
+// Animazione all'onload, ho utilizzato l'addEventListener e come primo parametro il load
 window.addEventListener("load", () => {
   let ritardo = 0;
 
+  // utilizzo un forEach
   sections.forEach((section) => {
+    // setTimeout non lo abbiamo ancora fatto, ma cercando su internet ho capito il suo funzionamento e ho cercato di utilizzarlo
     setTimeout(() => {
       section.classList.add("reveal");
     }, ritardo);
